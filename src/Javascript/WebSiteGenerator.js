2a82d74fe1acac15b937dba1e2155b19a706b207
md222zv <89812908+Mazender@users.noreply.github.com>
2023-09-10 16:55:38 +0200

    Try to fix GenerteNav

--- src/Javascript/WebSiteGenerator.js
+++ src/Javascript/WebSiteGenerator.js
@@ -11,10 +11,44 @@ generateHeader(headerText) {
   }
 
 
-
+  // Function to generate 
 generateFooter(footerText) {
   const customFooter = document.createElement('footer');
   customFooter.innerHTML = `<p> &copy; ${new Date().getFullYear()} ${footerText} </p>`; 
   return customFooter;
   }
-}
+
+  // Function to generate 
+generateMainContent(mainTitle, mainText) {
+  const mainContent = document.createElement('main');
+  const content = document.createElement('div');
+  content.innerHTML = `
+    <h2>${mainTitle}</h2>
+    <p> ${mainText} </p>
+    `;
+
+  mainContent.appendChild(content);
+  return mainContent;
+  }
+
+  generateNav() {
+    const customNav = document.createElement('nav')
+    const customUl = document.createElement('ul')
+
+    const customLi = document.createEleme
+    const a = document.createElement('a')
+
+    customLi.appendChild(a)
+    customUl.appendChild(customLi);
+
+
+    customNav.appendChild(customUl);
+
+    return customNav;
+    
+    }
+
+    
+  } 
+
+
